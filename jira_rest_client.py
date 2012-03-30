eb4aba4becff82522170974ce4f43fd02af473ad
David Black <dblack@atlassian.com>
2012-03-30 13:29:51 +1100

    * Add an exception class.  * raise an exception if a http code >= 400 is returned. * add put, post, delete and get methods. * add a  _get_connection_if_none_provided utility method.
    Signed-off-by: David <db@d1b.org>

--- jira_rest_client.py
+++ jira_rest_client.py
@@ -4,6 +4,11 @@ import StringIO
 import urllib
 import json
 
+class JiraRestClientException(Exception):
+        def __init__(self, message, response=None):
+                Exception.__init__(self, message)
+                self.response = response
+
 class JiraRestClient(object):
 	def __init__(self, base_uri, username=None, password=None):
 		self.base_uri = base_uri
@@ -33,7 +38,7 @@ class JiraRestClient(object):
 				urllib.quote(self.password)) )
 		return _connection, _string_io
 
-	def fetch(self, uri, post_data=None, _connection=None):
+	def _fetch(self, uri, post_data=None,  _connection=None):
 		""" XXX: write methods that use this method! """
 		if _connection is None:
 			_connection, _string_io = self.setup_basic_authed_pycurl_connection()
@@ -47,6 +52,34 @@ class JiraRestClient(object):
 			_connection.setopt(pycurl.POSTFIELDS, post_data)
 
 		_connection.perform()
+		http_code = _connection.getinfo(pycurl.HTTP_CODE)
+		if http_code >= 400:
+			error_mesg = "api failure, http code = %s" % http_code
+			raise JiraRestClientException(error_mesg, _string_io.getvalue())
 
 		return _string_io.getvalue()
+
+	def _get_connection_if_none_provided(self, _connection):
+		if _connection is None:
+			_connection, _string_io = self.setup_basic_authed_pycurl_connection()
+		return _connection
+
+	def _base_fetch_wrapper(self, uri, post_data=None, _connection=None, http_method='GET'):
+		_connection = self._get_connection_if_none_provided(_connection)
+		_connection.setopt(pycurl.CUSTOMREQUEST, http_method)
+		return self._fetch(uri, None, _connection)
+
+	def get(self, uri, post_data=None, _connection=None):
+		return self._base_fetch_wrapper(uri, post_data, _connection, 'GET')
+
+	def post(self, uri, post_data=None, _connection=None):
+		_connection = self._get_connection_if_none_provided(_connection)
+		_connection.setopt(pycurl.POST, 1)
+		return self._fetch(uri, None, _connection)
+
+	def put(self, uri, post_data=None, _connection=None):
+		return self._base_fetch_wrapper(uri, post_data, _connection, 'PUT')
+
+	def delete(self, uri, post_data=None, _connection=None):
+		return self._base_fetch_wrapper(uri, post_data, _connection, 'DELETE')
 
